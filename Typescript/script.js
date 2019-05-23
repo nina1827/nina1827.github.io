eb0b264816c2c3755aea4b67d1d622b5fae4682e
nina1827 <48830860+nina1827@users.noreply.github.com>
2019-05-23 16:07:03 +0200

    tyaufgabe1

--- Typescript/script.js
+++ Typescript/script.js
@@ -1,3 +1,39 @@
-console.log("Test1");
-//hello world
+console.log("What an exciting website");
+window.alert("U clicked on my AMAZING website"); //bei öffnen der website ein alert
+window.onload = function () {
+    console.log("Buttooooooooons");
+    Rechnung();
+    document.getElementById("ID1").addEventListener("click", Click);
+    document.getElementById("ID2").addEventListener("click", addPara);
+    document.getElementById("ID3").addEventListener("click", classchange);
+};
+function Rechnung() {
+    let vorname = "KITKAT"; //let.variablenname:typ="mimimi";
+    console.log(vorname);
+    let nachname = "NINA";
+    console.log(nachname);
+    let number1 = 1827;
+    console.log(number1);
+    let number2 = 69;
+    console.log(vorname + nachname); //addition von 2 strings
+    console.log(number1 + number2); //addition von 2 numbers
+    console.log(vorname + number1); //addition von string und number
+}
+function Click() {
+    console.log("buttons are fab-fab-fabulous");
+    document.getElementById("ID1").innerHTML = "Good job u clicked me"; //änderung bei click
+}
+document.getElementById("ID2");
+function addPara() {
+    let newPara = document.createElement("P");
+    let position = document.getElementById("body");
+    position.appendChild(newPara);
+    newPara.innerHTML = "Another element appeared";
+    console.log("Woooooooooh another element!");
+}
+function classchange() {
+    console.log("I changed my class uwu");
+    document.getElementById("ID3").innerHTML = "I changed my class UwU";
+    document.getElementById("ID3").className = "new class!";
+}
 //# sourceMappingURL=script.js.map
--- Typescript/script.ts
+++ Typescript/script.ts
@@ -1,4 +1,54 @@
-console.log("Test1");
-//hello world
+console.log("What an exciting website"); 
+window.alert("U clicked on my AMAZING website"); //bei öffnen der website ein alert
+
+window.onload = function () //erscheint erst, sobald seite fertig geladen ist
+{
+  console.log("Buttooooooooons");
+  Rechnung();
+  document.getElementById("ID1").addEventListener("click", Click)
+  document.getElementById("ID2").addEventListener("click", addPara)
+  document.getElementById("ID3").addEventListener("click", classchange)
+}
 
 
+function Rechnung()
+{
+let vorname : string = "KITKAT"; //let.variablenname:typ="mimimi";
+console.log(vorname);
+let nachname : string ="NINA"; 
+console.log(nachname);
+let number1 : number = 1827;
+console.log(number1);
+let number2 : number = 69;
+console.log(vorname+nachname); //addition von 2 strings
+console.log(number1+number2); //addition von 2 numbers
+console.log(vorname+number1); //addition von string und number
+}
+
+function Click()
+{
+    console.log("buttons are fab-fab-fabulous");
+    document.getElementById("ID1").innerHTML="Good job u clicked me" //änderung bei click
+
+}
+
+
+document.getElementById("ID2")
+
+
+function addPara() // generieren von neuen Elementen
+{
+  let newPara = document.createElement("P");
+  let position = document.getElementById("body")  
+  position.appendChild(newPara);
+  newPara.innerHTML="Another element appeared";
+  console.log("Woooooooooh another element!")
+}
+
+function classchange() //ändert klasse eines html objekts
+{
+console.log("I changed my class uwu")
+document.getElementById("ID3").innerHTML="I changed my class UwU";
+
+document.getElementById("ID3").className="new class!"
+}
