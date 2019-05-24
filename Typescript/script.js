bb0f344d40aa172412c45681982e11b677bbc10e
nina1827 <48830860+nina1827@users.noreply.github.com>
2019-05-24 12:32:12 +0200

    ?

--- Typescript/script.js
+++ Typescript/script.js
@@ -5,28 +5,35 @@ window.onload = function () {
     document.getElementById("ID1").addEventListener("click", Click);
     document.getElementById("ID2").addEventListener("click", addPara);
     document.getElementById("ID3").addEventListener("click", classchange);
-    let buttonelement = document.createElement("button");
-    buttonelement.innerHTML = "Klick mich";
+    let buttonelement = document.createElement("button"); //neuen Button generieren, ohne in html zu erstellen
+    buttonelement.innerHTML = "Try me ~ Ill show u some numbers hehe";
     buttonelement.addEventListener("click", Rechnung);
     document.getElementById("test").appendChild(buttonelement);
 };
+function Click() {
+    console.log("buttons are fab-fab-fabulous");
+    document.getElementById("ID1").innerHTML = "Good job u clicked me"; //änderung bei click
+}
 function Rechnung() {
-    let vorname = "KITKAT"; //let variablenname:typ="mimimi";
+    let vorname = "KITKAT"; //let variablenname:typ="mimimi"
     console.log(vorname);
     let nachname = "NINA";
     console.log(nachname);
     let number1 = 1827;
     console.log(number1);
     let number2 = 69;
+    number1 = 2718;
+    console.log(number1);
     console.log(number2);
     console.log(vorname + nachname); //addition von 2 strings
     console.log(number1 + number2); //addition von 2 numbers
     console.log(vorname + number1); //addition von string und number
     document.getElementById("ID3").innerHTML = number2.toString();
 }
-function Click() {
-    console.log("buttons are fab-fab-fabulous");
-    document.getElementById("ID1").innerHTML = "Good job u clicked me"; //änderung bei click
+function classchange() {
+    console.log("I changed my class uwu");
+    document.getElementById("ID3").innerHTML = "I changed my class UwU";
+    document.getElementById("ID3").className = "new class!";
 }
 function addPara() {
     let newPara = document.createElement("P");
@@ -34,10 +41,5 @@ function addPara() {
     position.appendChild(newPara);
     newPara.innerHTML = "Surprise!!! Another element appeared";
     console.log("Woooooooooh another element!");
-}
-function classchange() {
-    console.log("I changed my class uwu");
-    document.getElementById("ID3").innerHTML = "I changed my class UwU";
-    document.getElementById("ID3").className = "new class!";
 }
 //# sourceMappingURL=script.js.map
--- Typescript/script.ts
+++ Typescript/script.ts
@@ -7,27 +7,10 @@ window.onload = function () //erscheint erst, sobald seite fertig geladen ist
   document.getElementById("ID1").addEventListener("click", Click)
   document.getElementById("ID2").addEventListener("click", addPara)
   document.getElementById("ID3").addEventListener("click", classchange)
-  let buttonelement:HTMLElement = document.createElement("button");
-  buttonelement.innerHTML = "Klick mich";
+  let buttonelement:HTMLElement = document.createElement("button"); //neuen Button generieren, ohne in html zu erstellen
+  buttonelement.innerHTML = "Try me ~ Ill show u some numbers hehe";
   buttonelement.addEventListener("click", Rechnung);
   document.getElementById("test").appendChild(buttonelement);
-}
-
-
-function Rechnung()
-{
-let vorname : string = "KITKAT"; //let variablenname:typ="mimimi";
-console.log(vorname);
-let nachname : string ="NINA"; 
-console.log(nachname);
-let number1 : number = 1827;
-console.log(number1);
-let number2 : number = 69;
-console.log(number2);
-console.log(vorname+nachname); //addition von 2 strings
-console.log(number1+number2); //addition von 2 numbers
-console.log(vorname+number1); //addition von string und number
-document.getElementById("ID3").innerHTML = number2.toString();
 }
 
 function Click()
@@ -37,6 +20,31 @@ function Click()
 
 }
 
+function Rechnung()
+{
+  let vorname : string = "KITKAT"; //let variablenname:typ="mimimi"
+  console.log(vorname);
+  let nachname : string ="NINA"; 
+  console.log(nachname);
+  let number1 : number = 1827;
+  console.log(number1);
+  let number2 : number = 69;
+  number1 =2718;
+  console.log(number1);
+  console.log(number2);
+  console.log(vorname+nachname); //addition von 2 strings
+  console.log(number1+number2); //addition von 2 numbers
+  console.log(vorname+number1); //addition von string und number
+  document.getElementById("ID3").innerHTML = number2.toString();
+}
+
+
+function classchange() //ändert klasse eines html objekts
+{
+  console.log("I changed my class uwu")
+  document.getElementById("ID3").innerHTML="I changed my class UwU";
+  document.getElementById("ID3").className="new class!"
+}
 function addPara() // generieren von neuen Elementen
 {
   let newPara = document.createElement("P");
@@ -44,12 +52,4 @@ function addPara() // generieren von neuen Elementen
   position.appendChild(newPara);
   newPara.innerHTML="Surprise!!! Another element appeared";
   console.log("Woooooooooh another element!")
-}
-
-function classchange() //ändert klasse eines html objekts
-{
-console.log("I changed my class uwu")
-document.getElementById("ID3").innerHTML="I changed my class UwU";
-
-document.getElementById("ID3").className="new class!"
 }
