cd19ddd24fe0e68977541f0311f0a5c5dfd6d5e7
nina1827 <48830860+nina1827@users.noreply.github.com>
2019-05-29 16:49:41 +0200

    what did i just do

---
+++ TS Aufgabe_02/script2.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+window.alert("Clicking on this page will destroy ur pc");
+window.onload = function () {
+    document.getElementById("button1").addEventListener("mouseover", buggybutton);
+};
+function buggybutton() {
+    document.getElementById("button1").innerHTML = "skrrrr bugs";
+}
+//# sourceMappingURL=script2.js.map
